bb5c6e8b2fc2a55ed6e8c66197cf2d7429ec4ec0
lucastavareslogicalis <lucas.tavares@la.logicalis.com>
2024-08-27 11:18:35 -0300

    comit final

---
+++ amplify/amplify_ui/src/main.ts
@@ -0,0 +1,26 @@
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
+//Add Existing AWS Resources - https://docs.amplify.aws/react/build-a-backend/add-aws-services/rest-api/existing-resources/
+const existingConfig = Amplify.getConfig();
+Amplify.configure({
+  ...existingConfig,
+  API: {
+    ...existingConfig.API,
+    REST: {
+      ...existingConfig.API?.REST,
+      AssistantConfigApi: {
+        endpoint:
+          'https://grxgoi4uja.execute-api.us-east-1.amazonaws.com/',
+        region: 'us-east-1'
+      }
+    }
+  }
+});
+
+createApp(App).mount("#app");
+
